3e3803c2762e02d087d9d94618260ddf68b8c308
carsantir1 <csantostirado99@gmail.com>
2021-02-10 19:14:28 +0100

    her

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -161,7 +161,7 @@ APIS = {}
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
-BASEURL="https://egc-carsantir-examen2.herokuapp.com/"
+BASEURL="https://egc-carsantir-examen.herokuapp.com/"
 try:
     from local_settings import *
 except ImportError:
