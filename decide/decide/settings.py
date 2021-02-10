06ea053f93633bb81cac1e8e83a5c155df522557
carsantir1 <csantostirado99@gmail.com>
2021-02-10 22:45:58 +0100

    porf

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -107,10 +107,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'decide',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
